369b443fec90a327f021d4a54885b78d53c241d1
dAEcoder <ashleesean2015@gmail.com>
2026-01-25 17:50:35 +0800

    static muna telemed, charting, at settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import DashboardLayout from './layouts/DashboardLayout';
-import Overview from './pages/Overview'; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Overview from "./pages/Overview";
+import Telemed from "./pages/Telemed";
+import Charting from "./pages/Charting";
+import Settings from "./pages/Settings";
 
 //  placeholder
 const Beds = () => <div>Bed Management Page</div>;
@@ -10,8 +13,11 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<Overview />} /> 
+          <Route index element={<Overview />} />
           <Route path="beds" element={<Beds />} />
+          <Route path="telemed" element={<Telemed />} />
+          <Route path="charting" element={<Charting />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
